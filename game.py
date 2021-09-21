7a7a58dd8dbc7357c4725ba25b2e5bfce2e25f59
virginiasara12 <89534393+virginiasara12@users.noreply.github.com>
2021-09-21 12:15:17 -0400

    Update game.py

--- game.py
+++ game.py
@@ -8,6 +8,11 @@ print("Rock, Paper, Scissors, Shoot!")
 
 #x = input("Choose 'rock' or 'paper' or 'scissors'")
 user_choice = input("Choose 'rock' or 'paper' or 'scissors': ")
+
+if user_choice not in ["rock", "paper", "scissors"]:
+    print("Sorry, invalid input. Please try again.")
+    exit()
+
 print(user_choice)
 
 # computer choice (at random)
@@ -17,3 +22,26 @@ options = ["rock", "paper", "scissors"]
 computer_choice = random.choice(options)
 print("Computer chose:")
 print(computer_choice)
+
+if user_choice == computer_choice:
+    print("It's a tie!")
+
+if user_choice == "rock" and computer_choice == "scissors":
+    print("Congrats! You won!")
+if user_choice == "rock" and computer_choice == "paper":
+    print("Oh, the computer won. It's ok.")
+
+
+if user_choice == "paper" and computer_choice == "scissors":
+    print("Oh, the computer won. It's ok.")
+if user_choice == "paper" and computer_choice == "rock":
+    print("Congrats! You won!")
+
+
+if user_choice == "scissors" and computer_choice == "rock":
+    print("Oh, the computer won. It's ok.")
+if user_choice == "scissors" and computer_choice == "paper":
+    print("Congrats! You won!")
+
+
+print("Thanks for playing. Please play again!")
